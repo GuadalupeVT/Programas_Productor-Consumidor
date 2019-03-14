6a08d323659afdc82682d5f8bd933de85ce51a65
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-14 11:13:30 -0600

    Se agrego la interface Buffer al ejemplo 1

--- Sesion17_ProductorConsumidorWS/src/Prueba.java
+++ Sesion17_ProductorConsumidorWS/src/Prueba.java
@@ -1,3 +1,19 @@
+// Fig. 23.9: Buffer.java
+// Buffer interface specifies methods called by Producer and Consumer.
+//La interfaz de búfer especifica métodos llamados por el Productor y el Consumidor.
+interface Buffer{
+// place int value into Buffer
+//colocar valor int en Buffer
+	public void blockingPut(int value) throws InterruptedException;
+// return int value from Buffer
+//devuelve el valor int desde Buffer
+	public int blockingGet() throws InterruptedException;
+} // end interface Buffer
+
+
+
+
+
 
 public class Prueba {
 

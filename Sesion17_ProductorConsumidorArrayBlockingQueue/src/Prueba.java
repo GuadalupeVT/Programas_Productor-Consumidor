9ef0d6ba681d23a3c451ac1efdd6d724d1318456
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-14 12:24:15 -0600

    Se agrego la clase BlockingBuffer

--- Sesion17_ProductorConsumidorArrayBlockingQueue/src/Prueba.java
+++ Sesion17_ProductorConsumidorArrayBlockingQueue/src/Prueba.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
 
 interface Buffer{
 // place int value into Buffer
@@ -11,7 +13,32 @@ interface Buffer{
 //Producer with a run method that inserts the values 1 to 10 in buffer.
 //Productor con un método de ejecución que inserta los valores 1 a 10 en el búfer.
 
-
+class BlockingBuffer implements Buffer{
+	private final ArrayBlockingQueue<Integer> buffer; // shared buffer
+	                                                  //buffer compartido
+	public BlockingBuffer(){
+		buffer = new ArrayBlockingQueue<Integer>(1);
+	}
+	
+	// place value into buffer
+	//pone el valor en el buffer
+	public void blockingPut(int value) throws InterruptedException{
+		buffer.put(value); // place value in buffer
+		                   //pone el valor en el buffer 
+		//System.out.printf("%s%2d\t%s%d%n", "Producer writes ", value,"Buffer cells occupied: ", buffer.size());
+		System.out.printf("%s%2d\t%s%d%n", "Productor escribio", value,"Buffer cells ocupada: ", buffer.size());
+	}
+	
+	// return value from buffer
+	//regresa un valor del buffer
+	public int blockingGet() throws InterruptedException{
+		int readValue = buffer.take(); // remove value from buffer
+		                               // remover valores del buffer
+		//System.out.printf("%s %2d\t%s%d%n", "Consumer reads ",readValue, "Buffer cells occupied: ", buffer.size());
+		System.out.printf("%s %2d\t%s%d%n", "Consumidor leyo",readValue, "Buffer cells ocupada: ", buffer.size());
+		return readValue;
+	}
+} // end class BlockingBuffer
 
 
 public class Prueba {
